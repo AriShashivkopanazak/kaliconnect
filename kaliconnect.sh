7c227594a75e8f94236ee932faee04914f82b92a
root <root@localhost.localdomain>
2019-08-14 16:55:03 -0700

    one step at a time

--- kaliconnect.sh
+++ kaliconnect.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-/sbin/iw dev
-echo "what interface do you want to use?"
-read 'interface: ' interface
+ip a
+
+echo "what wireless interface do you want to use?"
+echo "eg: wlan0"
+read -p "interface: " interface
 echo "setting up $interface"
 sudo ip link set $interface up
-sudo /sbin/iw $interface scan | less
-read 'Desired SSID: ' ssid
-echo "please put your password below"
-sudo wpa_passphrase $ssid >> /etc/wpa_supplicant.conf
-sudo wpa_supplicant -B -i $interface -c /etc/wpa_supplicant.conf
-/sbin/iw $interface link
-sudo dhclient $interface
-echo "test to see if it worked:"
-echo "$ ping www.google.com"
+iw $interface scan | less
