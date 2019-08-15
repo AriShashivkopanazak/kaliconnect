9ab2883632e63d4409034c1f65eef7b5ee6c0314
Ari Shashivkopanazak <ari.shashivkopanazak@gmail.com>
2019-08-15 14:18:41 -0700

    main body

--- kaliconnect.sh
+++ kaliconnect.sh
@@ -1,17 +1,33 @@
-# Author: Ari Shashivkopanazak
 #!/bin/bash
-ip a
 
+#lists all interfaces
+ip a
 echo "what wireless interface do you want to use?"
 echo "eg: wlan0"
 read -p "interface: " interface
 echo "setting up $interface"
+
+#sets up desired interface
 sudo ip link set $interface up
-iw $interface scan | less
 
+# scans for available networks
+# iw $interface scan | less
 
+# echo "what Network do you want to connect to? (case sensitive)"
+# read -p "Network: " $network
+# echo "please put your password below"
 
-# #KaliConnect banner, comment out if you do not want
+# puts credentials in file
+# wpa_passphrase $network >> /etc/wpa_supplicant.conf
+
+# connects to access point
+# wpa_supplicant -B -i $interface -c /etc/wpa_supplicant.conf
+
+# obtains ip address
+# dhclient $interface
+
+# clears screen
+# clear
 # echo "                                                                                          "
 # echo "                                                                                          "
 # echo "                         888888:                                                          "
@@ -25,7 +41,5 @@ iw $interface scan | less
 # echo " 8  8Z   88 O8     88       8    O8       8    8   8  78   8   8  O8___88  8         8    "
 # echo "888  88  88888O  888888  O88888   888888  88888   888 88  888 8O8  88888   O888D8    88888"
 
-
-#Acknowledgements:
-#Thank you developers of WPA_Supplicant!  Could not have done it without you guys.
-#Finally, Thank you glassgiant.com for designing the banner!
+# shows info about your connected access point
+# ip addr show $interface
