8060d8a61cb722be28fcc81e9b863ac4ecbdad87
AriShashivkopanazak <ari.shashivkopanazak@gmail.com>
2019-08-21 03:09:42 +0000

    successful

--- kaliconnect.sh
+++ kaliconnect.sh
@@ -11,37 +11,40 @@ echo "setting up $interface"
 sudo ip link set $interface up
 
 # scans for available networks
-# iw $interface scan | less
+iwlist $interface scan | grep ESSID
 
 # echo "what Network do you want to connect to? (case sensitive)"
-# read -p "Network: " $network
-# echo "please put your password below"
+read -p "Network: " $network
+echo "put your Password below"
 
+wpa_passphrase "$network" >> list
+wpa_supplicant -B -i $interface -c list
 # puts credentials in file
-# wpa_passphrase $network >> /etc/wpa_supplicant.conf
+
+
 
 # connects to access point
-# wpa_supplicant -B -i $interface -c /etc/wpa_supplicant.conf
+#wpa_supplicant -B -i $interface -c /etc/wpa_supplicant.conf
 
 # obtains ip address
-# dhclient $interface
+dhclient $interface
 
 # clears screen
-# clear
+clear
 
 
-# echo "                                                                                          "
-# echo "                                                                                          "
-# echo "                         888888:                                                          "
-# echo "                      ?88I    =888                                                        "
-# echo "                      8 :888888+ 88                                                       "
-# echo "                       O8+     88                                                         "
-# echo "                         888O88                                                           "
-# echo "                  888      88                                                             "
-# echo "O88 8O8            88             88~ZD8                                             D    "
-# echo " 8O8      O888     88     888    O7       O88D8   88D88Z  888888  :88888   78O888  O88888 "
-# echo " 8  8Z   88 O8     88       8    O8       8    8   8  78   8   8  O8___88  8         8    "
-# echo "888  88  88888O  888888  O88888   888888  88888   888 88  888 8O8  88888   O888D8    88888"
+echo "                                                                                          "
+echo "                                                                                          "
+echo "                         888888:                                                          "
+echo "                      ?88I    =888                                                        "
+echo "                      8 :888888+ 88                                                       "
+echo "                       O8+     88                                                         "
+echo "                         888O88                                                           "
+echo "                  888      88                                                             "
+echo "O88 8O8            88             88~ZD8                                             D    "
+echo " 8O8      O888     88     888    O7       O88D8   88D88Z  888888  :88888   78O888  O88888 "
+echo " 8  8Z   88 O8     88       8    O8       8    8   8  78   8   8  O8___88  8         8    "
+echo "888  88  88888O  888888  O88888   888888  88888   888 88  888 8O8  88888   O888D8    88888"
 
 # shows info about your connected access point
 # ip addr show $interface
