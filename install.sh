53a6532568bb325e6ebee3dc3d23486cd3675cb7
root <root@localhost.localdomain>
2019-10-01 17:51:54 +0000

    install script ready for testing

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# Do not run if you want to test
 echo "welcome to kaliconnect!"
-# moves script out of repo and removes repo
+echo "this only works if you are root user :)"
+mv kaliconnect.sh /root
 cd ..
-mv ./kaliconnect/kaliconnect.sh ./kaliconnect.sh
 rm -r ./kaliconnect
-./kaliconnect.sh
+cd /root
+alias kalicon = "/root/kaliconnect.sh"
+clear
+echo "done! excecute "kalicon" as root user to initate kaliconnect"
